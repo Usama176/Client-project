be9c2dfc0a349c5e077f61907f154f047a88223d
Usama176 <usamaarifshah7@gmail.com>
2021-08-06 10:43:15 +0500

    Project Delivered

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import BecomeAnArtistPage from "./pages/becomeAnArtistPage/BecomAnArtistPage";
 import './App.css';
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/">
-          <BecomeAnArtistPage/>
-        </Route>
-      </div>
+      <Switch>
+        <div className="App">
+          <Route exact path="/">
+            <BecomeAnArtistPage/>
+          </Route>
+        </div>
+      </Switch>
     </Router>
   );
 }
