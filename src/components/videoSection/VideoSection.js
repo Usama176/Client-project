20e8b4be9ebbcf23af3432f3e837c0928ded849a
Usama Arif Shah <usamaarifshah7@gmail.com>
2021-08-10 17:12:52 +0500

    Fixed

--- src/components/videoSection/VideoSection.js
+++ src/components/videoSection/VideoSection.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import VideoPlayer from 'react-video-js-player';
+import { Player, BigPlayButton, PosterImage } from 'video-react';
+import "../../../node_modules/video-react/dist/video-react.css";
 import './videoSection.css';
-import Thumbnail from '../../assets/images/thumbnail.jpg';
 import MyVideo from '../../assets/videos/video.mp4';
+import Thumbnail from '../../assets/images/thumbnail.jpg';
 
 const VideoSection = () => {
 
@@ -12,11 +13,17 @@ const VideoSection = () => {
     return (
         <div className="videoSec">
             <div className="videoSec_Body">
-                <VideoPlayer 
+                <Player
                     src={videoSrc}
                     poster={poster}
-                    className="videoSec_Video"
-                />
+                    fluid = {false}
+                    width="100%"
+                    height="auto"
+                    className="video"
+                >
+                    <PosterImage className="poster"/>
+                    <BigPlayButton className="big-play-button" position="center" />
+                </Player>
                 <div className="videoSec_TxtBtnCont">
                     <div className="videoSec_Text">
                     <h4>“Literature, painting, music - the most basic lesson that all art teaches us is to stop, look, and listen to life on this planet, including our own lives, as a vastly richer, deeper, more mysterious business than most of the time it ever occurs to us to suspect as we bumble along from day to day on automatic pilot.”</h4>
